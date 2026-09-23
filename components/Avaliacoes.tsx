5a65061699051dd1c839756310ec648e0f179948
Marcio78MF <151024172+Marcio78MF@users.noreply.github.com>
2026-09-22 23:48:25 -0700

    Add files via upload

--- components/Avaliacoes.tsx
+++ components/Avaliacoes.tsx
@@ -15,7 +15,7 @@ const AVALIACOES = [
   {
     nome: 'Cliente P.',
     iniciais: 'CP',
-    comentario: 'O atendimento do doutor Márcio França é nota 10. Eu super recomendo.',
+    comentario: 'O atendimento do doutor Márcio Jr. França é nota 10. Eu super recomendo.',
     nota: 5,
     data: 'Cliente do Google'
   },
